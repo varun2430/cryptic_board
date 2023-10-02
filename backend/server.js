a20cd0c9cded6014fe038a6dbc76cf16d23a7e86
Varun Kadkade <varunkadkade2442@gmail.com>
2023-10-02 22:30:27 +0530

    http -> https

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+import https from "https";
 import express from "express";
 import { S3Client } from "@aws-sdk/client-s3";
 import mongoose from "mongoose";
@@ -11,6 +13,9 @@ import fileRoutes from "./routes/file.js";
 import replyRoutes from "./routes/reply.js";
 
 dotenv.config();
+
+const key = fs.readFileSync("./key.pem");
+const cert = fs.readFileSync("./cert.pem");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -41,10 +46,12 @@ app.use("/api/post", postRoutes);
 app.use("/api/file", fileRoutes);
 app.use("/api/reply", replyRoutes);
 
+const server = https.createServer({ key: key, cert: cert }, app);
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log(`server running on port ${process.env.PORT}`);
     });
   })
